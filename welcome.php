ca1897f526589d0df45e508c35e6efe811c747e1
Aditya Subanji <153857055+AdityaSubanji@users.noreply.github.com>
2023-12-24 11:07:43 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,62 @@
+
+<html>
+
+<head>
+
+
+<div class="container">
+   
+    <div class="content">
+	
+	<h3>This is your Ticket<span>
+	<a href="login.php" class="btn">login</a>
+	<a href="register.php" class="btn">register</a>
+	<a href="logout.php" class="btn">logout</a>
+
+    </div>
+
+</div>	
+
+
+</head>
+
+
+<body>
+<style>
+*
+{
+	font-family: 'poppins',sans-serif;
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+	outline:none;
+	border:none;
+	text-decoration:none;
+}
+
+.container
+{
+min-height:100vh;
+display:flex;
+align-item:center;
+justify-content:center;
+padding:20px;
+padding-bottom:60px;	
+}
+
+.container .content
+{
+ text-align:center;	
+}
+
+
+.container .content h3
+{
+	font-size:30px;
+	color:#333;welconmr
+}
+
+</style>
+</body>
+
+</html>
